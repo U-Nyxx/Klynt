206d8ddf9464853b88ee85e80afc26a759f7e577
U-Nyxx <U-Nyxx@users.noreply.github.com>
2026-09-11 23:32:59 +0700

    fix: Kotlin 2.4.20 for Gradle 9 support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,9 +7,9 @@ val localProps = Properties().apply {
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android") version "2.2.10"
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10"
+    id("org.jetbrains.kotlin.android") version "2.4.20"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.20"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.20"
 }
 
 android {
@@ -78,7 +78,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "2.2.10"
+        kotlinCompilerExtensionVersion = "2.4.20"
     }
 }
 
