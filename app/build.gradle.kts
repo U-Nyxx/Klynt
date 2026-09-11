4a07d7e57c7c870fa42df574dbac4e8cf4deb498
U-Nyxx <U-Nyxx@users.noreply.github.com>
2026-09-11 23:35:40 +0700

    fix: drop kotlin.android plugin (built into AGP 9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,6 @@ val localProps = Properties().apply {
 
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android") version "2.4.20"
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.20"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.4.20"
 }
